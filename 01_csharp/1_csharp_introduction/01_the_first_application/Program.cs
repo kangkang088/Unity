05f2c054ceb36590cf0e1bcfad5b2b1d9c05daaa
kangkang088 <1808131844@qq.com>
2026-03-21 09:40:50 +0800

    csharp入门-01_the_first_application结束。

---
+++ 01_csharp/1_csharp_introduction/01_the_first_application/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01_the_first_application
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Write("你好");
+            Console.WriteLine("Hello");
+            Console.Write("你好");
+
+            if(Console.ReadLine() == "c")
+            {
+                Console.WriteLine("c");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
