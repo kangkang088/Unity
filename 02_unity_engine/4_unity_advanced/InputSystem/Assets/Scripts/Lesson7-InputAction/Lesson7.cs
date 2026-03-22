6e936a2e3db1fd0519bcc2d1d6886fc7c4a2d030
kangkang088 <1808131844@qq.com>
2026-03-22 23:45:56 +0800

    input system-inputaction 讲解未完。

--- 02_unity_engine/4_unity_advanced/InputSystem/Assets/Scripts/Lesson7-InputAction/Lesson7.cs
+++ 02_unity_engine/4_unity_advanced/InputSystem/Assets/Scripts/Lesson7-InputAction/Lesson7.cs
@@ -5,15 +5,38 @@ using UnityEngine.InputSystem;
 public class Lesson7 : MonoBehaviour
 {
     public InputAction move;
-    public InputAction fire;
+    public InputAction axis;
+    public InputAction axis_2d;
+    public InputAction axis_3d;
+    public InputAction double_button;
+    public InputAction three_button;
+
     private void Start()
     {
         // InputAction类-对前面那些设备类的封装,可视化
-        move.Enable();
+        // move.Enable();
 
         move.started += Started;
         move.performed += Performed;
         move.canceled += Canceled;
+
+        // axis.Enable();
+        // axis_2d.Enable();
+        // axis_3d.Enable();
+        // double_button.Enable();
+        double_button.performed += Performed;
+
+        three_button.Enable();
+        three_button.performed += Performed;
+    }
+
+    private void Update()
+    {
+        Debug.Log(axis.ReadValue<float>());
+
+        Debug.Log(axis_2d.ReadValue<Vector2>());
+
+        Debug.Log(axis_3d.ReadValue<Vector3>());
     }
 
     private void Canceled(InputAction.CallbackContext context)
