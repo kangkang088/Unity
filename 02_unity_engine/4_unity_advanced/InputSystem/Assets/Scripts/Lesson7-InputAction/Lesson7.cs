24a461dd3d50ca951b1fcae29ddf5efda37aaaca
kangkang088 <1808131844@qq.com>
2026-03-22 19:32:06 +0800

    input system-inputaction 讲解未完。

--- 02_unity_engine/4_unity_advanced/InputSystem/Assets/Scripts/Lesson7-InputAction/Lesson7.cs
+++ 02_unity_engine/4_unity_advanced/InputSystem/Assets/Scripts/Lesson7-InputAction/Lesson7.cs
@@ -9,5 +9,52 @@ public class Lesson7 : MonoBehaviour
     private void Start()
     {
         // InputAction类-对前面那些设备类的封装,可视化
+        move.Enable();
+
+        move.started += Started;
+        move.performed += Performed;
+        move.canceled += Canceled;
+    }
+
+    private void Canceled(InputAction.CallbackContext context)
+    {
+        Debug.Log("Canceled");
+
+        if (context.phase == InputActionPhase.Canceled)
+        {
+            Debug.Log("Phase Canceled");
+        }
+    }
+
+    private void Performed(InputAction.CallbackContext context)
+    {
+        Debug.Log("Performed");
+
+        if (context.phase == InputActionPhase.Performed)
+        {
+            Debug.Log("Phase Performed");
+            // Debug.Log(context.performed);
+            Debug.Log(context.duration);
+        }
+    }
+
+    private void Started(InputAction.CallbackContext context)
+    {
+        Debug.Log("Started");
+
+        if (context.phase == InputActionPhase.Started)
+        {
+            Debug.Log("Phase Started");
+
+            // Debug.Log(context.action.name);
+
+            // Debug.Log(context.control.name);
+
+            // Debug.Log(context.ReadValue<float>());
+
+            // Debug.Log(context.duration);
+
+            // Debug.Log(context.startTime);
+        }
     }
 }
