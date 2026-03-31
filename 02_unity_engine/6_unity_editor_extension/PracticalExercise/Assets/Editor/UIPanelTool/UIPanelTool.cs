7e76b4d160ff282f8c0598a88b1ab20954ebbcc5
kangkang088 <1808131844@qq.com>
2026-03-31 20:59:11 +0800

    practical exercise done

--- 02_unity_engine/6_unity_editor_extension/PracticalExercise/Assets/Editor/UIPanelTool/UIPanelTool.cs
+++ 02_unity_engine/6_unity_editor_extension/PracticalExercise/Assets/Editor/UIPanelTool/UIPanelTool.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using TMPro;
 using UnityEditor;
+using UnityEditor.Compilation;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using Assembly = System.Reflection.Assembly;
 
 namespace Editor.UIPanelTool
 {
@@ -12,10 +16,18 @@ namespace Editor.UIPanelTool
         private readonly List<string> _filterNames = new()
         {
             "Image", "Text (TMP)", "RawImage", "Background", "Checkmark", "Label", "Text (Legacy)", "Arrow",
-            "Placeholder", "Fill", "Handle", "Viewport"
+            "Placeholder", "Fill", "Handle", "Viewport", "Scrollbar Vertical", "Scrollbar Horizontal"
         };
 
         private Dictionary<string, Type> _controlType = new();
+
+        private string _panelBaseScript;
+        private string _panelScript;
+
+        private string _panelName;
+
+        private Vector2 _scrollPos1;
+        private Vector2 _scrollPos2;
 
         [MenuItem("GameObject/UI/自动生成")]
         public static void ShowWindow()
@@ -28,10 +40,58 @@ namespace Editor.UIPanelTool
 
         private void InitInfo()
         {
+            _panelBaseScript = "";
+            _panelScript = "";
+
+            _controlType.Clear();
+
+            _scrollPos1 = Vector2.zero;
+            _scrollPos2 = Vector2.zero;
+
             var obj = Selection.activeGameObject;
             if (!obj) return;
 
-            var btnInfo = FindControl<Button>(obj);
+            _panelName = obj.name;
+
+            var controlStrInfo = new ControlStrInfo();
+
+            var btnControlInfo = FindControl<Button>(obj);
+            if (btnControlInfo == null) return;
+            controlStrInfo += btnControlInfo;
+            btnControlInfo = FindControl<Toggle>(obj);
+            if (btnControlInfo == null) return;
+            controlStrInfo += btnControlInfo;
+            btnControlInfo = FindControl<Slider>(obj);
+            if (btnControlInfo == null) return;
+            controlStrInfo += btnControlInfo;
+            btnControlInfo = FindControl<InputField>(obj);
+            if (btnControlInfo == null) return;
+            controlStrInfo += btnControlInfo;
+            btnControlInfo = FindControl<Dropdown>(obj);
+            if (btnControlInfo == null) return;
+            controlStrInfo += btnControlInfo;
+            btnControlInfo = FindControl<ScrollRect>(obj);
+            if (btnControlInfo == null) return;
+            controlStrInfo += btnControlInfo;
+            btnControlInfo = FindControl<Text>(obj);
+            if (btnControlInfo == null) return;
+            controlStrInfo += btnControlInfo;
+            btnControlInfo = FindControl<Image>(obj);
+            if (btnControlInfo == null) return;
+            controlStrInfo += btnControlInfo;
+            btnControlInfo = FindControl<RawImage>(obj);
+            if (btnControlInfo == null) return;
+            controlStrInfo += btnControlInfo;
+            btnControlInfo = FindControl<TextMeshProUGUI>(obj);
+            if (btnControlInfo == null) return;
+            controlStrInfo += btnControlInfo;
+
+            var baseStr = AssetDatabase.LoadAssetAtPath<TextAsset>("Assets/Editor/UIPanelTool/UIConfigBase.txt");
+            _panelBaseScript = string.Format(baseStr.text, obj.name, controlStrInfo.nameStr, controlStrInfo.findStr,
+                controlStrInfo.listenerStr, controlStrInfo.funcStr);
+
+            var str = AssetDatabase.LoadAssetAtPath<TextAsset>("Assets/Editor/UIPanelTool/UIConfig.txt");
+            _panelScript = string.Format(str.text, obj.name, obj.name);
         }
 
         private ControlStrInfo FindControl<T>(GameObject obj) where T : UIBehaviour
@@ -60,7 +120,7 @@ namespace Editor.UIPanelTool
 
                 info.nameStr += $"public {typeof(T).Name} {controls[i].gameObject.name};\n\t";
                 info.findStr +=
-                    $"{controls[i].gameObject.name} = transform.Find(\"{controls[i].gameObject.name}\").GetComponent<{typeof(T).Name}>();\n\t\t";
+                    $"{controls[i].gameObject.name} = transform.Find(\"{GetPath(controls[i].transform, obj.transform)}\").GetComponent<{typeof(T).Name}>();\n\t\t";
 
                 switch (typeof(T).Name)
                 {
@@ -87,14 +147,72 @@ namespace Editor.UIPanelTool
             return info;
         }
 
+        private string GetPath(Transform obj, Transform panelTrans)
+        {
+            var path = obj.name;
+
+            while (obj.transform.parent != panelTrans)
+            {
+                path = obj.parent.name + "/" + path;
+                obj = obj.parent;
+            }
+
+            return path;
+        }
+
         private void OnGUI()
         {
+            if (string.IsNullOrEmpty(_panelBaseScript))
+            {
+                GUILayout.Label("存在同名同类型控件，请先解决");
+                return;
+            }
+
+            _scrollPos1 = EditorGUILayout.BeginScrollView(_scrollPos1);
+
+            GUILayout.Label(_panelBaseScript);
+
+            EditorGUILayout.EndScrollView();
+
+            GUILayout.Label("----------------------------------------------");
+
+            _scrollPos2 = EditorGUILayout.BeginScrollView(_scrollPos2);
+
+            GUILayout.Label(_panelScript);
+
+            EditorGUILayout.EndScrollView();
+
             if (GUILayout.Button("自动生成脚本"))
             {
-                var path = EditorUtility.SaveFilePanel("保存自动生成的脚本", Application.dataPath, "", "cs");
+                var path = EditorUtility.SaveFilePanel("保存自动生成的脚本", Application.dataPath + "/Scripts/",
+                    _panelName + "Base", "cs");
                 if (!string.IsNullOrEmpty(path))
                 {
+                    File.WriteAllText(path, _panelBaseScript);
+                    // path = path.Replace("Base", "");
+                    var index = path.LastIndexOf("Base", StringComparison.Ordinal);
+                    path = path[..index] + ".cs";
+                    if (!File.Exists(path))
+                        File.WriteAllText(path, _panelScript);
+
+                    CompilationPipeline.assemblyCompilationFinished += CompilationPipelineOnAssemblyCompilationFinished;
+
+                    AssetDatabase.Refresh();
                 }
+            }
+        }
+
+        private void CompilationPipelineOnAssemblyCompilationFinished(string arg1, CompilerMessage[] arg2)
+        {
+            if (arg1.Contains("Assembly-CSharp.dll"))
+            {
+                var assembly =
+                    Assembly.LoadFrom(Application.dataPath.Substring(0,
+                                          Application.dataPath.LastIndexOf("Assets", StringComparison.Ordinal)) +
+                                      arg1);
+                Selection.activeGameObject.AddComponent(assembly.GetType(_panelName));
+                Debug.Log(_panelName);
+                CompilationPipeline.assemblyCompilationFinished -= CompilationPipelineOnAssemblyCompilationFinished;
             }
         }
     }
